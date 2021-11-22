66e95b839d2bd5fe78563004b7a622cdef9b903e
William Thompson <ThomWill130@GoTVDSB.ca>
2021-11-22 09:59:59 -0500

    Add project files.

---
+++ projet.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "What is your name?";
+    cin >> name;
+    cout << "Hello " + name + " and welcome to the ICS3U quiz."
+
+}
+
