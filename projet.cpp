1f884f30ee3f6f297029b4ffa7002214c055813e
William Thompson <ThomWill130@GoTVDSB.ca>
2021-11-26 10:53:24 -0500

    main strcture 8/10 q

--- projet.cpp
+++ projet.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <string>
 #include <windows.h>  
-#include <conio.h>
 using namespace std;
 
 //global variables
@@ -17,20 +16,60 @@ void scoreDisplay()
     cout << "\n\n\n\nNext question:\n\n";
 }
 
+void colourChange(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+//grades score and provides a letter mark (challenge)
+void markGrader(int mark)
+{
+    if (mark > 10 || mark < 0)
+    {
+         cout << "There was somehow an error in marking your test.";
+    }
+    else if (mark == 10)
+    {
+        cout << "Your grade for this test: S (100%)";
+    }
+    else if (mark >= 8)
+    {
+        cout << "Your grade for this test: A (8+ correct answers)";
+    }
+    else if (mark == 7)
+    {
+        cout << "Your grade for this test: B (7 correct answers)";
+    }
+    else if (mark == 6)
+    {
+        cout << "Your grade for this test: C (6 correct answers)";
+    }
+    else if (mark == 5)
+    {
+        cout << "Your grade for this test: D (5 correct answers)";
+    }
+    else if (mark < 5)
+    {
+        cout << "Your grade for this test: F (under 5 correct answers - failed";
+    }
+}
+
 //function to check if the answer is correct, return the point value and display correct/incorrect
 int isCorrect(char ans, char correctAns, int pointValue)
 {
     questCount += 1;
     if (ans == correctAns)
     {
+        colourChange(2);
         cout << "Correct!\n\n";
+        colourChange(15);
         answerCount += pointValue;
         correctQCount += 1;
         return pointValue;
     }
-    textcolor(RED);
+    colourChange(4);
     cout << "Incorrect.\n\n";
-    system("Color 07");
+    colourChange(15);
     return 0;
 }
 //duplicating previous function | function overloading (string)
@@ -39,12 +78,16 @@ int isCorrect(string ans, string correctAns, int pointValue)
     questCount += 1;
     if (ans == correctAns)
     {
+        colourChange(2);
         cout << "Correct!\n\n";
+        colourChange(15);
         answerCount += pointValue;
         correctQCount += 1;
         return pointValue;
     }
+    colourChange(4);
     cout << "Incorrect.\n\n";
+    colourChange(15);
     return 0;
 }
 //duplicating previous function | function overloading (int)
@@ -53,12 +96,16 @@ int isCorrect(int ans, int correctAns, int pointValue)
     questCount += 1;
     if (ans == correctAns)
     {
+        colourChange(2);
         cout << "Correct!\n\n";
+        colourChange(15);
         answerCount += pointValue;
         correctQCount += 1;
         return pointValue;
     }
+    colourChange(4);
     cout << "Incorrect.\n\n";
+    colourChange(15);
     return 0;
 }
 // previous function but modified for multiple possible answers
@@ -67,12 +114,16 @@ int isCorrectMulti(string ans, string correctAns, string correctAns2, string cor
     questCount += 1;
     if ((ans == correctAns) || (ans == correctAns2) || (ans == correctAns3))
     {
+        colourChange(2);
         cout << "Correct!\n\n";
+        colourChange(15);
         answerCount += pointValue;
         correctQCount += 1;
         return pointValue;
     }
+    colourChange(4);
     cout << "Incorrect.\n\n";
+    colourChange(15);
     return 0;
 }
 
@@ -109,15 +160,19 @@ int main()
         cout << "Congratulations for not living under a rock.\n";
         break;
     case 'B':
+        colourChange(4);
         cout << "Do you live under a rock?\n";
         break;
     case 'C':
+        colourChange(4);
         cout << "Wrong country.\n";
         break;
     case 'D':
+        colourChange(4);
         cout << "Very close, better luck next time!\n";
         break;
     default:
+        colourChange(4);
         cout << "You inputted an invalid answer.\n";
     }
     isCorrect(response2, 'A', 10);
@@ -148,14 +203,34 @@ int main()
     string response6;
     cout << "Can fish drown?";
     cin >> response6;
-    isCorrect(response6, "YES", 25);
+    isCorrect(response6, "YES", 15);
     scoreDisplay();
 
-    //question seven (char + int selection question)
-    //?????????????????
+    //question seven (multiple choice char)
+    char response7;
+    cout << "Multiple choice: which of these animals CAN NOT drown?\n\nA: Seagulls\nB: Dogs\nC: Fish\nD: None of the above\n";
+    cin >> response7;
+    isCorrect(response7, 'D', 30);
+    scoreDisplay();
     
-    //question eight
+    //question eight (multiple choice int)
+    char response8;
+    cout << "Which one of these numbers is above 5? Enter one number: 4, 2, 5, or 7";
+    cin >> response8;
+    isCorrect(response8, '7', 20);
+    scoreDisplay();
+
+    //question nine
+
+    //question ten
+
+    //outro
+
+
+
+
+
+
 }
 
 
-https://www.youtube.com/watch?v=MvX4tVETjHk USE THIS FOR COLOUR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*****
